9e9b14ce5d320bcf606fe170581ead5eaf339735
samuelboada21 <samuelomarboba@ufps.edu.co>
2025-08-26 16:33:24 -0500

    Iniciando scaffold-base

---
+++ applications/app-service/src/main/java/co/com/microauth/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package co.com.microauth.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "co.com.microauth.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
